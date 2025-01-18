161b7c73d4221c0a3a4087ff814a3e5de852cdc0
Isaac Oduh <ufuomaoduh@gmail.com>
2025-01-18 18:16:12 +0000

    savepoint: implement logout and profile skelecton

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 // import { useState } from 'react';
-// import logo from '@assets/images/logo.svg';
+import logo from '@assets/images/logo.svg';
 // import { FaCaretDown, FaCaretUp, FaRegBell, FaRegEnvelope } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,13 +11,35 @@ import {
   DropdownMenuTrigger
 } from '@components/ui/dropdown-menu';
 
-import { Avatar, AvatarFallback, AvatarImage } from '@components/ui/avatar';
+import { Avatar, AvatarImage, AvatarFallback } from '@components/ui/avatar';
+import useLocalStorage from '@hooks/useLocalStorage';
+import useSessionStorage from '@hooks/useSessionStorage';
 
 import { Button } from '@components/ui/button';
 import HeaderSkeleton from './HeaderSkeleton';
+import { Utils } from '@services/utils/utils.service';
+import { userService } from '@services/api/user.service';
+import { useNavigate } from 'react-router-dom';
 const Header = () => {
   const { profile } = useSelector((state) => state.user);
+
+  const storedUsername = useLocalStorage('username', 'get');
+  const [deleteStorageUsername] = useLocalStorage('username', 'delete');
+  const [setLoggedIn] = useLocalStorage('keepLoggedIn', 'set');
+  const [deleteSessionPageReload] = useSessionStorage('pageReload', 'delete');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   //   const [settings, setSettings] = useState([]);
+  const onLogout = async () => {
+    try {
+      setLoggedIn(false);
+      Utils.clearStore({ dispatch, deleteStorageUsername, deleteSessionPageReload, setLoggedIn });
+      await userService.logoutUser();
+      navigate('/');
+    } catch (error) {
+      Utils.dispatchNotification(error.response.data.message, 'error', dispatch);
+    }
+  };
   return (
     <>
       {!profile ? (
@@ -28,7 +50,7 @@ const Header = () => {
             <div className="flex justify-between h-16">
               <div className="flex">
                 <div className="flex-shrink-0 flex items-center">
-                  <img className="h-8 w-auto" src="" alt="logo" />
+                  <img className="h-8 w-auto" src={logo} alt="logo" />
                 </div>
               </div>
               <div className="flex items-center">
@@ -36,23 +58,23 @@ const Header = () => {
                   <DropdownMenuTrigger asChild>
                     <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                       <Avatar className="h-8 w-8">
-                        <AvatarImage src="" alt="@avatar" />
-                        <AvatarFallback>SC</AvatarFallback>
+                        <AvatarImage src={profile?.profilePicture} alt="@avatar" />
+                        <AvatarFallback>{profile?.username.charAt(0)}</AvatarFallback>
                       </Avatar>
                     </Button>
                   </DropdownMenuTrigger>
                   <DropdownMenuContent className="w-56" align="end" forceMount>
                     <DropdownMenuLabel className="font-normal">
                       <div className="flex flex-col space-y-1">
-                        <p className="text-sm font-medium leading-none">shadcn</p>
-                        <p className="text-xs leading-none text-muted-foreground">m@example.com</p>
+                        <p className="text-sm font-medium leading-none">@{storedUsername}</p>
+                        <p className="text-xs leading-none text-muted-foreground">{profile?.email}</p>
                       </div>
                     </DropdownMenuLabel>
                     <DropdownMenuSeparator />
                     <DropdownMenuItem>Profile</DropdownMenuItem>
                     <DropdownMenuItem>Settings</DropdownMenuItem>
                     <DropdownMenuSeparator />
-                    <DropdownMenuItem>Log out</DropdownMenuItem>
+                    <DropdownMenuItem onSelect={onLogout}>Log out</DropdownMenuItem>
                   </DropdownMenuContent>
                 </DropdownMenu>
               </div>
